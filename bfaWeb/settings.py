50ab07532094a6f13035bd64f609b27d71884f62
Ajingo <68058091+Ajingo2000@users.noreply.github.com>
2024-07-03 20:03:54 +0200

    Update settings.py

--- bfaWeb/settings.py
+++ bfaWeb/settings.py
@@ -16,7 +16,8 @@ import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
