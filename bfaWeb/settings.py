09b13a358fb58baf441c08ddb44cc72331202ff5
Ajingo <68058091+Ajingo2000@users.noreply.github.com>
2024-07-03 18:06:08 +0200

    Update settings.py

--- bfaWeb/settings.py
+++ bfaWeb/settings.py
@@ -14,9 +14,9 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -174,7 +174,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Add the path to your React build folder
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
