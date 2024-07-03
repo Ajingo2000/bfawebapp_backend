f23436ca37247b47100e557881605990f4dc0f28
Ajingo <68058091+Ajingo2000@users.noreply.github.com>
2024-07-03 15:42:19 +0200

    Update settings.py AND ADDED A DIFFERENT BASE_DIR CODE

--- bfaWeb/settings.py
+++ bfaWeb/settings.py
@@ -14,9 +14,9 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
